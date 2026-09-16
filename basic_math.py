1f65be23d80640669a0d955eb1f00a3c716f2321
Mignonne Gihozo <Mignonne.Gihozo@stu.ocsb.ca>
2026-09-16 13:33:17 +0000

    Added basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created By: Mignonne Gihozo
+# Date: Sep. 16, 2026
+# This program does basic math .
+
+
+def main():
+    print("7 - 3 = {}".format(7 - 3))  # subtraction
+    print("2 + 4  = {}".format(2 + 4))  # addition
+    print("8 * 6 = {}".format(8 * 6))  # multiplication
+    print("4 / 2 = {}".format(4 / 2))  # division
+    print("2 + 5 *3 = {}".format(2 + 5 * 3))  # bedmas
+    print("2^3 = {}".format(2 ^ 3))  # bitwise operator
+    print("2**3 = {}".format(2**3))  # exponent
+    print("16**0.5 = {}".format(16**0.5))  # square root
+    print("10 % 7 = {}".format(10 % 7))  # modulo remainder
+
+
+if __name__ == "__main__":
+    main()
